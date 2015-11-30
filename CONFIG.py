a9c5b44b97bd618f238570128aee3b96951ce386
rghusbands <rghusbands@comcat.net>
2015-11-30 14:43:23 -0800

    added cgi script

--- CONFIG.py
+++ CONFIG.py
@@ -6,12 +6,12 @@ Edit to fit development or deployment environment.
 import random
 
 ### My local development environment
-#PORT=5000
-#DEBUG = True
+PORT=5000
+DEBUG = True
 #MONGO_URL = "mongodb://memo:iremember@localhost:27333/memos"  # on Gnat
 
 ### On ix.cs.uoregon.edu (Michal Young's instance of MongoDB)
 #PORT=random.randint(5000,8000)
-PORT=6996
-DEBUG = False # Because it's unsafe to run outside localhost
+#PORT=6995
+#DEBUG = False # Because it's unsafe to run outside localhost
 MONGO_URL =  "mongodb://dbuser:password1@localhost:4898/memos"  # on ix
